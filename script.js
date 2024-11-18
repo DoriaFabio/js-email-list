7f55f50b4d68a9eac40c8784da2f36295a175ba5
DoriaFabio <doriafabio2001@gmail.com>
2024-11-18 17:53:19 +0100

    create li with email generate

--- script.js
+++ script.js
@@ -6,3 +6,19 @@
 // - Abbellire con CSS o Bootstrap
 // - Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le altre)
 
+const elUl = document.querySelector("ul");
+
+
+axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((res) => {
+    console.log(res.data.response);
+    const email = res.data.response; 
+    elUl.innerHTML = `<li class="list-group-item">${email}</li>`;
+})
+
+    .catch((error) => {
+        console.log(error);
+    })
+
+    .finally(() => {
+        console.log("in tutti i casi eseguita");
+    });
